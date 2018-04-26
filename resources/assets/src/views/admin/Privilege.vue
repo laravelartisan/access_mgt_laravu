b7d82fad3504673c24efefa0583a818696539d96
Md Nazim Mahmud Khan <rajaftfl@outlook.com>
2018-04-26 19:47:42 +0600

    sub menu dropdown added in privilege page

--- resources/assets/src/views/admin/Privilege.vue
+++ resources/assets/src/views/admin/Privilege.vue
@@ -24,7 +24,7 @@
 
                                             </select>
                                         </div>
-                                       
+
                                         <div class="col-lg-2">
                                             <label class="col-lg-12 col-form-label form-control-label">Copy From</label>
                                             <select class="form-control form-control-sm"  >
@@ -42,6 +42,54 @@
 
                                         <div class="col-lg-2">
                                             <label class="col-lg-12 col-form-label form-control-label">Permission Level</label>
+                                            <select class="form-control form-control-sm"  >
+                                                <option value="" disabled>Select Role</option>
+                                                <option value="" >Selective</option>
+                                                <option value="" >Full</option>
+
+                                            </select>
+                                        </div>
+                                    </div>
+
+
+
+                                </div>
+                                <div class="card-body">
+
+                                    <div class="form-group row">
+                                        <div class="menu-dropdown">
+                                            <label class="menu-dropdown-level col-form-label form-control-label">Role</label>
+                                            <select class="form-control form-control-sm"  >
+                                                <option value="" disabled>Select Role</option>
+
+                                            </select>
+                                        </div>
+                                        <template ></template>
+                                        <div class="menu-dropdown">
+                                            <label  class="menu-dropdown-level col-form-label form-control-label">Module</label>
+                                            <select class="form-control form-control-sm"  >
+                                                <option value="" disabled>Select Module</option>
+
+                                            </select>
+                                        </div>
+
+                                        <div class="menu-dropdown">
+                                            <label class="menu-dropdown-level col-form-label form-control-label">Copy From</label>
+                                            <select class="form-control form-control-sm"  >
+                                                <option value="" disabled>Select Role</option>
+
+                                            </select>
+                                        </div>
+                                        <div class="menu-dropdown">
+                                            <label class="menu-dropdown-level col-form-label form-control-label">Inherit From</label>
+                                            <select class="form-control form-control-sm"  >
+                                                <option value="" disabled>Select Role</option>
+
+                                            </select>
+                                        </div>
+
+                                        <div class="menu-dropdown">
+                                            <label class="menu-dropdown-level col-form-label form-control-label">Permission Level</label>
                                             <select class="form-control form-control-sm"  >
                                                 <option value="" disabled>Select Role</option>
                                                 <option value="" >Selective</option>
@@ -72,3 +120,79 @@
 
     </main>
 </template>
+
+<script>
+
+    export default {
+
+        data:function(){
+            return{
+                menus: [
+                    {
+                        name: 'Knit Garments',
+                        id:1,
+                        children: [
+                            {
+                                name: 'Order Tracking',
+                                id: 2,
+                                children: [
+                                    { name: 'Sales Forecast Entry', id:3},
+                                    { name: 'Quotation Inquery', id:4 },
+
+                                ]
+                            },
+                            { name: 'Fabric Booking', id: 5 },
+                            { name: 'Trims Booking', id: 6 },
+                            { name: 'Service Booking', id: 7 },
+                        ],
+                    },
+                    {
+                        name: 'My Tree',
+                        id: 8,
+                        children: [
+                            { name: 'hello', id: 9 },
+                            { name: 'wat', id: 10 },
+                            {
+                                name: 'child folder',
+                                id: 11,
+                                children: [
+                                    {
+                                        name: 'child folder',
+                                        id: 12,
+                                        children: [
+                                            { name: 'hello', id: 13 },
+                                            { name: 'wat', id: 14 }
+                                        ]
+                                    },
+                                    { name: 'hello', id: 15 },
+                                    { name: 'wat', id: 16 },
+                                    {
+                                        name: 'child folder',
+                                        id: 17,
+                                        children: [
+                                            { name: 'hello', id: 18 },
+                                            { name: 'wat', id: 19 }
+                                        ]
+                                    }
+                                ]
+                            },
+                            { name: 'Logic', id: 20 },
+                            { name: 'Platform', id: 21 },
+                        ],
+                    },
+                ]
+
+            }
+        },
+    }
+
+</script>
+
+<style scoped>
+    .menu-dropdown{
+        width: 14%;
+    }
+    .menu-dropdown-level{
+        width: 100%;
+    }
+</style>
---
+++ resources/assets/src/widgets/privileges/Menu.vue
@@ -0,0 +1,48 @@
+<template>
+    <div v-if="hasChildren" class="menu-dropdown">
+        <label  class="menu-dropdown-level col-form-label form-control-label">Sub Menu</label>
+        <select class="form-control form-control-sm"  >
+            <option value="" disabled>Select Module</option>
+            <option v-for="(menuChild, index) in menu.children" :value="menuChild.id" disabled>{{ menuChild.name }}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'privilege',
+
+        computed: {
+
+            hasChildren: function(){
+                return this.menu.children && this.menu.children.length
+            }
+        },
+
+        data: function() {
+            return{
+                menuId: '',
+                menu:
+                {
+                        name: 'Knit Garments',
+                        id:1,
+                        children: [
+                            {
+                                name: 'Order Tracking',
+                                children: [
+                                    { name: 'Sales Forecast Entry1', id: 5},
+                                    { name: 'Sales Forecast Entry2', id: 6},
+                                    { name: 'Sales Forecast Entry3', id: 7},
+
+                                ]
+                            },
+                            { name: 'Fabric Booking', id: 2 },
+                            { name: 'Trims Booking', id: 3 },
+                            { name: 'Service Booking', id: 4 },
+                        ],
+                    },
+
+            }
+        }
+    }
+</script>
